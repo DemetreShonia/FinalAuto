787811c3d305e314115e3ef864d3dd5dc759989b
DemetreShonia <shonka.demeloper@gmail.com>
2023-05-28 22:55:19 +0400

    Product List Fetched

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+
+interface Data {
+  man_id: number;
+  man_name: string;
+  is_car: number;
+  is_spec: number;
+  is_moto: number;
+}
+
 function App() {
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://static.my.ge/myauto/js/mans.json");
+      const json = await response.json();
+      setData(json);
+      console.log(json);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
   return <>zdd</>;
 }
 
