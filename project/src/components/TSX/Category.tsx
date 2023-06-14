c3b821f36a9f1d086d721bd13c0ed4c534ff9489
MariMamgo <114804409+MariMamgo@users.noreply.github.com>
2023-06-14 20:19:06 +0400

    .

--- project/src/components/TSX/Category.tsx
+++ project/src/components/TSX/Category.tsx
@@ -1,26 +1,95 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { BsCheck } from "react-icons/bs";
 import { IoIosArrowDown } from "react-icons/io";
 
+interface CheckboxState {
+  კატეგორია1: boolean;
+  კატეგორია2: boolean;
+  კატეგორია3: boolean;
+  კატეგორია4: boolean;
+}
+
 const Category: React.FC = () => {
   const [drop, setDrop] = useState<boolean>(false);
+  const [checkboxes, setCheckboxes] = useState<CheckboxState>({
+    კატეგორია1: false,
+    კატეგორია2: false,
+    კატეგორია3: false,
+    კატეგორია4: false,
+  });
+
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    setCheckboxes({ ...checkboxes, [name]: checked });
+  };
+
+  const setAllFalse = () => {
+    setCheckboxes({
+      კატეგორია1: false,
+      კატეგორია2: false,
+      კატეგორია3: false,
+      კატეგორია4: false,
+    });
+  };
+
+  const getActiveCheckboxStrings = () => {
+    const activeCheckboxes = Object.entries(checkboxes)
+      .filter(([name, checked]) => checked)
+      .map(([name]) => name);
+    // make string out of activeCheckboxes
+    if (activeCheckboxes.length >= 1) {
+      let res = activeCheckboxes.join(", ");
+      // if res is too long, cut it and add '...'
+      if (res.length > 18) {
+        res = res.slice(0, 18) + "...";
+      }
+      return res;
+    }
+  };
+
+  const getActiveCheckboxes = () => {
+    const activeCheckboxes = Object.entries(checkboxes)
+      .filter(([name, checked]) => checked)
+      .map(([name]) => name);
+    return activeCheckboxes;
+  };
+  const activeList = getActiveCheckboxStrings();
 
   return (
     <div className="CategoryContainer">
       <div className="Category" onClick={() => setDrop(!drop)}>
-        <div className="Category-t"> კატეგორია </div>
+        <div className="Category-t">
+          {activeList ? activeList : "კატეგორია"}
+        </div>
         <div className={drop ? "arrow rotate" : "arrow"}>
           <IoIosArrowDown />
         </div>
       </div>
       {drop && (
         <div className="DropDown">
-          <div className="options">
+          <div className="Modeloptions">
             <div className="checkbox-container">
-              <div className="checkboxes">
-                <input type="checkbox" />
-                <input type="checkbox" />
-                <input type="checkbox" />
+              <div className="Modelcheckboxes">
+                {Object.entries(checkboxes).map(([name, checked]) => (
+                  <div className="checkboxCover">
+                    <div
+                      className={checked ? "checker checkedd" : "checker"}
+                      onClick={() =>
+                        handleCheckboxChange({
+                          target: { name, checked: !checked },
+                        })
+                      }
+                    >
+                      <BsCheck />
+                    </div>
+                    <div
+                      key={name}
+                      className={checked ? "checkbox checked!" : "checkbox"}
+                    >
+                      {name}
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
             <div className="emptyFilter">
--- project/src/components/TSX/Model.tsx
+++ project/src/components/TSX/Model.tsx
@@ -1,26 +1,94 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { BsCheck } from "react-icons/bs";
 import { IoIosArrowDown } from "react-icons/io";
 
+interface CheckboxState {
+  მოდელი1: boolean;
+  მოდელი2: boolean;
+  მოდელი3: boolean;
+  მოდელი4: boolean;
+  მოდელი5: boolean;
+}
+
 const Model: React.FC = () => {
   const [drop, setDrop] = useState<boolean>(false);
+  const [checkboxes, setCheckboxes] = useState<CheckboxState>({
+    მოდელი1: false,
+    მოდელი2: false,
+    მოდელი3: false,
+    მოდელი4: false,
+    მოდელი5: false,
+  });
+
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    setCheckboxes({ ...checkboxes, [name]: checked });
+  };
+
+  const setAllFalse = () => {
+    setCheckboxes({
+      მოდელი1: false,
+      მოდელი2: false,
+      მოდელი3: false,
+      მოდელი4: false,
+      მოდელი5: false,
+    });
+  };
+
+  const getActiveCheckboxStrings = () => {
+    const activeCheckboxes = Object.entries(checkboxes)
+      .filter(([name, checked]) => checked)
+      .map(([name]) => name);
+    // make string out of activeCheckboxes
+    if (activeCheckboxes.length >= 1) {
+      let res = activeCheckboxes.join(", ");
+      // if res is too long, cut it and add '...'
+      if (res.length > 18) {
+        res = res.slice(0, 18) + "...";
+      }
+      return res;
+    }
+    return "";
+  };
+
+  const getActiveCheckboxes = () => {
+    const activeCheckboxes = Object.entries(checkboxes)
+      .filter(([name, checked]) => checked)
+      .map(([name]) => name);
+    return activeCheckboxes;
+  };
+  const activeList = getActiveCheckboxStrings();
 
   return (
     <div className="ModelContainer">
       <div className="Model" onClick={() => setDrop(!drop)}>
-        <div className="Model-t"> მოდელი </div>
+        <div className="Model-t">{activeList ? activeList : "მოდელი"}</div>
         <div className={drop ? "arrow rotate" : "arrow"}>
           <IoIosArrowDown />
         </div>
       </div>
       {drop && (
         <div className="DropDown">
-          <div className="options">
+          <div className="Modeloptions">
             <div className="checkbox-container">
-              <div className="checkboxes">
-                <input type="checkbox" />
-                <input type="checkbox" />
-                <input type="checkbox" />
+              <div className="Modelcheckboxes">
+                {Object.entries(checkboxes).map(([name, checked]) => (
+                  <div className="checkboxCover" key={name}>
+                    <div
+                      className={checked ? "checker checkedd" : "checker"}
+                      onClick={() =>
+                        handleCheckboxChange({
+                          target: { name, checked: !checked },
+                        })
+                      }
+                    >
+                      <BsCheck />
+                    </div>
+                    <div className={checked ? "checkbox checked!" : "checkbox"}>
+                      {name}
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
             <div className="emptyFilter">
